24b532f6f3458e2cec43601effd24634355d1385
Julien Palard <julien@palard.fr>
2018-08-07 13:23:22 +0200

    When updating ace, also remove no-loger-existing upstream files.

--- bin/upgrade_ace.sh
+++ bin/upgrade_ace.sh
@@ -1,7 +1,4 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#!/bin/sh
 
 git clone https://github.com/ajaxorg/ace-builds.git /tmp/ace
-cd  "${DIR}/../django_ace/static/django_ace/ace"
-cp -R /tmp/ace/src/* ./
+rsync --delete -r /tmp/ace/src/ django_ace/static/django_ace/ace/
