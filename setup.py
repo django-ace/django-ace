ec18ee38bb4d91c57a58437b1756f5db58a3bd65
Julien Palard <julien@palard.fr>
2019-12-04 23:51:21 +0100

    Bump to version 1.0.6.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-ace",
-    version="1.0.5",
+    version="1.0.6",
     description="django-ace provides integration for ajax.org ACE with Django",
     long_description=open("README.rst").read(),
     author="Bradley Ayers",
