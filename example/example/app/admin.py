5be01b63f72bf3ccba9c1e6dca5cb8f589a82be3
Julien Palard <julien@palard.fr>
2025-02-18 12:15:32 +0100

    example: Add an admin interface with the widget in it.

--- example/example/app/admin.py
+++ example/example/app/admin.py
@@ -1,2 +1,22 @@
+from django import forms
 from django.contrib import admin
+from django_ace import AceWidget
 
+from .models import Snippet
+
+
+class SnippetForm(forms.ModelForm):
+    class Meta:
+        model = Snippet
+        fields = ("text",)
+        widgets = {
+            "text": AceWidget(
+                mode="markdown", theme="twilight", width=None, height=None
+            ),
+        }
+
+
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    form = SnippetForm
+    readonly_fields = ("created_at",)
--- example/example/app/models.py
+++ example/example/app/models.py
@@ -5,5 +5,8 @@ class Snippet(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.text.splitlines()[0]
+
     class Meta:
-        ordering = ('-created_at', )
+        ordering = ("-created_at",)
